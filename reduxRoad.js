38e00e8ce74fac8ebbcebb635d27bcbf01e5454b
rbngm <rabinghimire2012@gmail.com>
2023-02-14 11:29:24 +0800

    reducer case added

--- reduxRoad.js
+++ reduxRoad.js
@@ -1,7 +1,38 @@
-const initialWagonState = {};
+const initialWagonState = {
+  supplies: 100,
+  distance: 0,
+  days: 0,
+};
 
 const roadReducer = (state = initialWagonState, action) => {
   switch (action.type) {
+    case "gather": {
+      return {
+        ...state,
+        supplies: state.supplies + 15,
+        kilometer: state.distance,
+        days: state.days + 1,
+      };
+    }
+    case "travel": {
+      if (state.supplies - 20 * action.payload <= 0) {
+        return state;
+      }
+      return {
+        ...state,
+        supplies: state.supplies - 20 * action.payload,
+        kilometer: state.distance + 10 * action.payload,
+        days: state.days + action.payload,
+      };
+    }
+    case "tippedWagon": {
+      return {
+        ...state,
+        supplies: state.supplies - 30,
+        kilometer: state.distance,
+        days: state.days + 1,
+      };
+    }
     default:
       return state;
   }
